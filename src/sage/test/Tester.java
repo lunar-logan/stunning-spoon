56b632272998fa9242c5fbc97d05d9c9747a80a2
lunar-logan <mrprogrammar@gmail.com>
2016-03-03 20:19:52 +0530

    Completed Tester class definition

--- src/sage/test/Tester.java
+++ src/sage/test/Tester.java
@@ -1,10 +1,15 @@
 package sage.test;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import edu.stanford.nlp.ling.Sentence;
+import org.bson.Document;
 import sage.spi.Triplet;
+import sage.util.CryptoUtil;
 
-import java.util.List;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by Anurag Gautam on 03-03-2016.
@@ -17,6 +22,9 @@ public class Tester {
     private static final String DATABASE_NAME = "triple_db";
     private static final String COLLECTION_NAME = "triplets";
 
+    private int totalTested = 0;
+    private int correct = 0;
+
     public static Tester getInstance() {
         return instance;
     }
@@ -25,16 +33,65 @@ public class Tester {
         client = new MongoClient();
     }
 
-    private String sha1(String text) {
-
+    private String normalize(String text) {
+        return text.toLowerCase().replaceAll("\\w+", "").trim();
     }
 
-    public void add(Triplet t) {
-        MongoDatabase tripletDB = client.getDatabase(DATABASE_NAME);
-
+    public boolean insert(String origSentence, Triplet t) {
+        try {
+            String hash = CryptoUtil.md5(normalize(origSentence));
+            MongoDatabase tripletDB = client.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> collection = tripletDB.getCollection(COLLECTION_NAME);
+            Document doc = new Document(hash,
+                    new Document()
+                            .append("sub", Sentence.listToString(t.getSubject()))
+                            .append("pre", Sentence.listToString(t.getPredicate()))
+                            .append("obj", Sentence.listToString(t.getObject())));
+            collection.insertOne(doc);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
-    public void add(List<Triplet> tripletList) {
+    public void test(String origSentence, Triplet t) {
+        try {
+            String hash = CryptoUtil.md5(normalize(origSentence));
+            MongoDatabase tripletDB = client.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> collection = tripletDB.getCollection(COLLECTION_NAME);
+            Document doc = collection.find(Filters.eq("hash", hash)).first();
+            if (doc != null) {
+                String sub = Sentence.listToString(t.getSubject());
+                String pre = Sentence.listToString(t.getPredicate());
+                String obj = Sentence.listToString(t.getObject());
 
+                String subFound = (String) doc.get("sub");
+                String preFound = (String) doc.get("pre");
+                String objFound = (String) doc.get("obj");
+
+                if ((sub.equalsIgnoreCase(subFound) || sub.contains(subFound))
+                        && (pre.equalsIgnoreCase(preFound) || pre.contains(preFound))
+                        && (obj.equalsIgnoreCase(objFound) || obj.contains(objFound))) {
+                    correct++;
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        totalTested++;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public int getTotalTested() {
+        return totalTested;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Score: %s/%d\nAccuracy: %.04f\n", correct, totalTested, ((correct / (float) totalTested) * 100.0));
     }
 }
