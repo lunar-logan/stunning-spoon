8e881f6010adb88987869b16b7f2fb0d15e261f5
lunar-logan <mrprogrammar@gmail.com>
2016-03-03 19:20:57 +0530

    Added more methods to CryptoUtil class

--- src/sage/util/CryptoUtil.java
+++ src/sage/util/CryptoUtil.java
@@ -14,4 +14,16 @@ public class CryptoUtil {
         MessageDigest digest = MessageDigest.getInstance(algorithm);
         return new String(digest.digest(text.getBytes()));
     }
+
+    public static String sha1(String text) throws NoSuchAlgorithmException {
+        return hash("SHA-1", text);
+    }
+
+    public static String sha256(String text) throws NoSuchAlgorithmException {
+        return hash("SHA-256", text);
+    }
+
+    public static String md5(String text) throws NoSuchAlgorithmException {
+        return hash("MD5", text);
+    }
 }
