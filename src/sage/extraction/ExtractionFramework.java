71cc0ad198b833529615019f492344958e340f71
Anurag Gautam <Anurag Gautam>
2016-06-02 18:33:06 +0530

    Fixed EF

--- src/sage/extraction/ExtractionFramework.java
+++ src/sage/extraction/ExtractionFramework.java
@@ -4,24 +4,30 @@ package sage.extraction;
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.trees.TypedDependency;
 import sage.Vocabulary;
+import sage.util.LogUtil;
 import sage.util.SPOTriplet;
 import sage.util.Util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 /**
  * Created by Anurag Gautam on 18-04-2016.
  */
 public class ExtractionFramework {
+
+    private LogUtil L = new LogUtil();
+
     private final Collection<TypedDependency> dependencies;
     private final Vocabulary vocab;
+
     private IndexedWord subject = null;
     private IndexedWord predicate = null;
+
     private ArrayList<IndexedWord> objects = new ArrayList<>();
+    private ArrayList<IndexedWord> predicates = new ArrayList<>();
+
     private IndexedWord lastSubject = null;
 
     public ExtractionFramework(Collection<TypedDependency> dependencies, Vocabulary vocabulary) {
@@ -29,32 +35,36 @@ public class ExtractionFramework {
         this.vocab = vocabulary;
     }
 
+
     public ArrayList<SPOTriplet> getTriples() {
         ArrayList<SPOTriplet> triples = new ArrayList<>();
 
-        dependencies.stream()
-                .filter(td -> td.reln().getShortName().equalsIgnoreCase("nsubj") || td.reln().getShortName().equalsIgnoreCase("nsubjpass"))
-                .forEach(td -> {
-                    handleNominalSubject(td);
-                    ArrayList<IndexedWord> subjectPhrase = new ArrayList<>();
-                    subjectPhrase.addAll(getSubjectAttributes(subject));
-                    subjectPhrase.add(handleCoref(subject));
+        filterByRelation(dependencies.stream(), (td) -> {
+            handleNominalSubject(td);
 
-                    ArrayList<IndexedWord> predicatePhrase = new ArrayList<>();
-                    predicatePhrase.add(predicate);
-                    Util.addIfNotNull(predicatePhrase, getPredicateAttributes(predicate));
+            ArrayList<IndexedWord> subjectPhrase = new ArrayList<>();
+            subjectPhrase.addAll(getSubjectAttributes(subject));
+//            subjectPhrase.add(handleCoref(subject));
 
-                    Collections.sort(subjectPhrase);
-                    Collections.sort(predicatePhrase);
+            ArrayList<IndexedWord> predicatePhrase = new ArrayList<>();
+            predicatePhrase.addAll(predicates);
+            predicates.forEach(p -> Util.addIfNotNull(predicatePhrase, getPredicateAttributes(p)));
 
-                    objects.forEach(obj -> {
-                        ArrayList<IndexedWord> objectPhrase = new ArrayList<>();
-                        objectPhrase.add(obj);
-                        objectPhrase.addAll(findAttributes(obj));
-                        Collections.sort(objectPhrase);
-                        triples.add(new SPOTriplet(Arrays.asList(), subjectPhrase, predicatePhrase, objectPhrase));
-                    });
-                });
+            Collections.sort(subjectPhrase);
+            Collections.sort(predicatePhrase);
+
+
+            objects.forEach(obj -> {
+                ArrayList<IndexedWord> objectPhrase = new ArrayList<>();
+                objectPhrase.add(obj);
+                objectPhrase.addAll(findAttributes(obj));
+                Collections.sort(objectPhrase);
+
+                triples.add(new SPOTriplet(Arrays.asList(), subjectPhrase, predicatePhrase, objectPhrase));
+            });
+
+
+        }, "nsubj", "nsubjpass");
 
         return triples;
     }
@@ -70,65 +80,110 @@ public class ExtractionFramework {
         return prp;
     }
 
+    /**
+     * Extracts the attributes of a subject phrase
+     *
+     * @param subj
+     * @return
+     */
     private ArrayList<IndexedWord> getSubjectAttributes(IndexedWord subj) {
-        ArrayList<IndexedWord> attrs = new ArrayList<>();
+        ArrayList<IndexedWord> attrs = new ArrayList<>();/*
         dependencies.stream()
                 .filter(td -> td.gov().equals(subj))
-                .forEach(td -> attrs.add(td.dep()));
+                .forEach(td -> attrs.add(td.dep()));*/
         return attrs;
     }
 
     private void handleNominalSubject(TypedDependency td) {
-        IndexedWord gov = td.gov();
-        IndexedWord dep = td.dep();
+        IndexedWord gov = td.gov();                             // Get the governor
+        IndexedWord dep = td.dep();                             // Get the dependent
+        IndexedWord base = null;
 
-        // If the governor is a verb then we've found our predicate
-        if (gov.tag().startsWith("VB")) {
-            subject = dep;
-            predicate = gov;
+
+        subject = dep;                                          // Subject in all cases  is the dependent
+        if (gov.tag().startsWith("VB")) {                       // If the governor is a verb then we've found our predicate
+            predicates.add(gov);
+            base = gov;
         } else {
-            subject = dep;
             TypedDependency cop = getRelation("cop", gov);
             if (cop != null) {
-                if (cop.dep().tag().startsWith("VB")) { // found our predicate
-                    predicate = cop.dep();
-                    objects.add(cop.gov());
+                base = handleCopRelation(cop);
+            }
+        }
+        findObject(base);
+    }
+
+    /**
+     * Handles copular relations
+     *
+     * @param cop
+     * @return
+     */
+    private IndexedWord handleCopRelation(TypedDependency cop) {
+        IndexedWord dep = cop.dep();
+        if (dep.tag().startsWith("VB")) {                    // Dependent of cop is a verb, probably the predicate
+            predicates.add(dep);
+            predicates.add(cop.gov());
+            return cop.gov();
+        }
+        return null;
+    }
+
+
+    /**
+     * Extracts the object phrase form the sentence (typed dependencies of it)
+     */
+    private void findObject(IndexedWord base) {
+        IndexedWord object = null;
+        if (base == null) {
+            L.w("base word is null, cannot find object");
+        } else {
+            TypedDependency dobj = getRelation("dobj", base);
+            if (dobj != null) {
+                object = dobj.dep();
+            } else {
+                L.w("Could not find `dobj` relation, looking for nmod");  //
+                TypedDependency nmod = getRelation("nmod", base);
+                if (nmod != null) {
+                    object = nmod.dep();
+                    addCase(object);
+                } else {
+                    L.w("nmod relation could not be located. Bad day huh!");
                 }
             }
         }
 
-        if (predicate != null && subject != null)
-            findObjects();
-    }
-
-    private void findObjects() {
-        getRelations("dobj")
-                .filter(d -> d.gov().equals(predicate))
-                .forEach(d -> objects.add(d.dep()));
-
-        dependencies.stream()
-                .filter(td -> {
-                    return objects.contains(td.gov()) && td.reln().getShortName().equalsIgnoreCase("conj");
-                })
-                .forEach(td -> objects.add(td.dep()));
-
-    }
-
-
-    private Stream<TypedDependency> getRelations(String relnName) {
-        return dependencies
-                .stream()
-                .filter(td -> td.reln().getShortName().equalsIgnoreCase(relnName));
-    }
-
-    private TypedDependency getRelation(String relnName, IndexedWord governor) {
-        for (TypedDependency dependency : dependencies) {
-            if (dependency.reln().getShortName().equalsIgnoreCase(relnName) &&
-                    dependency.gov().equals(governor)) {
-                return dependency;
-            }
+        if (object != null) {
+            objects.add(object);
+            addConjunct(object);
         }
-        return null;
+    }
+
+    private void addConjunct(IndexedWord object) {
+        TypedDependency cc = getRelation("cc", object);
+        if (cc != null) {
+            String ccWord = cc.dep().word();
+            if (ccWord.startsWith("or") || ccWord.startsWith("and")) {
+                filterByRelation(dependencies.stream(), td -> {
+                    if (td.gov().equals(object)) {
+                        objects.add(td.dep());
+                    }
+                }, "conj");
+            } else {
+                L.i("Unknown ccWord: " + ccWord + " found, ignoring");
+            }
+        } else {
+            L.i("No cc relation for " + object);
+        }
+    }
+
+    private void addCase(IndexedWord nmodGov) {
+        TypedDependency aCase = getRelation("case", nmodGov);
+        if (aCase == null) {
+            L.i("No case relation for " + nmodGov + " found");
+        } else {
+            predicates.add(aCase.dep());
+        }
     }
 
     /**
@@ -186,7 +241,6 @@ public class ExtractionFramework {
                 });
 
         if (attrs.isEmpty()) {
-
             // Handle nmod relations
             dependencies.stream()
                     .filter(td -> td.reln().getShortName().equalsIgnoreCase("nmod"))
@@ -208,4 +262,35 @@ public class ExtractionFramework {
         }
         return attrs;
     }
+
+
+    private Stream<TypedDependency> getRelations(String relnName) {
+        return dependencies
+                .stream()
+                .filter(td -> td.reln().getShortName().equalsIgnoreCase(relnName));
+    }
+
+    private TypedDependency getRelation(String relnName, IndexedWord governor) {
+        relnName = relnName.toLowerCase();
+
+        for (TypedDependency dependency : dependencies) {
+            if (dependency.reln().getShortName().toLowerCase().startsWith(relnName) &&
+                    dependency.gov().equals(governor)) {
+                return dependency;
+            }
+        }
+        return null;
+    }
+
+    private void filterByRelation(Stream<TypedDependency> stream, Consumer<TypedDependency> callback, String... relns) {
+        stream.filter(td -> {
+            String shortName = td.reln().getShortName();
+            for (String r : relns) {
+                if (shortName.startsWith(r)) return true;
+            }
+            return false;
+        }).forEach(callback);                                   // All the filtered relations consumed by the callback
+    }
+
+
 }
---
+++ src/sage/util/LogUtil.java
@@ -0,0 +1,43 @@
+package sage.util;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+/**
+ * Created by Dell on 02-06-2016.
+ */
+public class LogUtil {
+    private static Logger L = Logger.getLogger("SageLog");
+
+    static {
+        L.setUseParentHandlers(false);
+        L.addHandler(new ConsoleHandler() {
+            @Override
+            public void publish(LogRecord record) {
+                System.out.println(
+                        record.getLevel().getName().charAt(0) + " " +
+                                "[" + record.getSourceClassName() + '#' + record.getSourceMethodName() + "]" +
+                                ": " + record.getMessage()
+                );
+            }
+        });
+    }
+
+    public void i(String msg) {
+        L.log(Level.INFO, msg);
+    }
+
+    public void w(String msg) {
+        L.warning(msg);
+    }
+
+    public void e(String msg) {
+        L.log(Level.SEVERE, msg);
+    }
+
+    public Logger l() {
+        return L;
+    }
+}
